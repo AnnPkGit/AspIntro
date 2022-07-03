2e583ac639b29b190c2b2e855029a20f747fa984
AnnPkGit <103180183+AnnPkGit@users.noreply.github.com>
2022-07-03 10:44:31 +0300

    1.07.2022 Changes. Work with files + password hash/salt

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -15,81 +16,26 @@ namespace Intro.Controllers
         private readonly ILogger<HomeController> _logger;
         private readonly RandomService _randomService;
         private readonly IHasher _hasher;
-        private readonly IDateTimeSeparated _dateTimeSeparated;
         private readonly DAL.Context.IntroContext _introContext;
 
-        public HomeController(ILogger<HomeController> logger,
+        public HomeController(              // Внедрение зависимостей
+            ILogger<HomeController> logger, // через конструктор
             RandomService randomService,
             IHasher hasher,
-            IDateTimeSeparated dateTimeSeparated,
-            DAL.Context.IntroContext introContext)
+            DAL.Context.IntroContext introContext)  
         {
             _logger = logger;
             _randomService = randomService;
             _hasher = hasher;
-            _dateTimeSeparated = dateTimeSeparated;
             _introContext = introContext;
-        }
-
-        public ViewResult Register()
-        {
-            return View();
-        }
-
-        [HttpPost]
-        public IActionResult RegUser(Models.RegUserModel UserData)
-        {
-            //return Json(UserData);
-            string[] err = new string[6];
-            string[] inputValues = new string[3];
-
-            inputValues[0] = UserData.RealName;
-            inputValues[1] = UserData.Login;
-            inputValues[2] = UserData.Email;
-
-            if (UserData == null)
-            {
-                err[0] = ("Некорректный вызов");
-            }
-            else
-            {
-                if (String.IsNullOrEmpty(UserData.RealName))
-                {
-                    err[1] = "Имя не может быть пустым";
-                }
-                if (String.IsNullOrEmpty(UserData.Login))
-                {
-                    err[2] = "Логин не может быть пустым";
-                }
-                if (String.IsNullOrEmpty(UserData.Password1))
-                {
-                    err[3] = "Первый пароль не может быть пустым";
-                }
-                if (String.IsNullOrEmpty(UserData.Password2))
-                {
-                    err[4] = "Второй пароль не может быть пустым";
-                }
-                if (String.IsNullOrEmpty(UserData.Email))
-                {
-                    err[5] = "Эмэйл не может быть пустым";
-                }
-            }
-            ViewData["err"] = err;
-            ViewData["inputValues"] = inputValues;
-            return View("Register");
         }
 
         public IActionResult Index()
         {
-            ViewData["rnd"] = _randomService.Integer;
+            ViewData["rnd"] = "<b>" + _randomService.Integer + "</b>";
             ViewBag.hash = _hasher.Hash("123");
             ViewData["UsersCount"] = _introContext.Users.Count();
-
-            ViewBag.date = _dateTimeSeparated.Date();
-            ViewBag.time = _dateTimeSeparated.Time();
-
             ViewBag.userNames = _introContext.Users.Select(user => user.RealName);
-
             return View();
         }
 
@@ -102,29 +48,103 @@ namespace Intro.Controllers
         {
             var model = new AboutModel
             {
-                Data = "Data"
+                Data = "The Model Data"
             };
-
             return View(model);
         }
 
-        public IActionResult Contacts()
+        public ViewResult Register()
         {
-            //создание объекта данные которого используются в View
-            var model = new ContactsModel
-            {
-                Adress = "221b Baker Street",
-                Phone = "+44 1632 960729",
-                Name = "Sherlock Holmes Investigation Bureau"
-            };
 
-            return View(model);
+            return View();
+        }
+
+        [HttpPost]  // Следующий метод срабатывает только на POST-запрос
+        // Метод может автоматически собрать все переданные данные в модель
+        //  по совпадению имен
+        public IActionResult RegUser(Models.RegUserModel UserData)
+        {
+            // return Json(UserData);  // способ проверить передачу данных
+            String[] err = new String[6];
+
+            if (UserData == null)
+            {
+                err[0] = "Некорректный вызов (нет данных)";
+            }
+            else
+            {
+                if (String.IsNullOrEmpty(UserData.Login))
+                {
+                    err[2] = "Логин не может быть пустым";
+                }
+                if (String.IsNullOrEmpty(UserData.Email))
+                {
+                    err[5] = "Email не может быть пустым";
+                }
+                if( UserData.Avatar != null )  // есть переданный файл
+                {
+                    // файл нужно сохранить в нужном месте.
+                    // для простоты доступа - в папку wwwroot/img
+                    // !! крайне НЕ рекомендуется сохранять имя переданного файла
+                    //  - возможен конфликт, если разные пользователи -- одно имя
+                    //  - уязвимость - если в имени файла есть ../
+
+                    // Д.З. Сформировать новое имя файла, сохранить расширение,
+                    //   убедиться, что файл не стирает к-то другой файл
+
+                    UserData.Avatar.CopyToAsync(
+                        new FileStream(
+                            "./wwwroot/img/" + UserData.Avatar.FileName, 
+                            FileMode.Create));
+                }
+                // если валидация пройдена, то добавляем пользователя в БД
+                // валидация успешна если нет сообщений об ошибках
+                bool isValid = true;
+                foreach (string error in err)
+                {
+                    if (!String.IsNullOrEmpty(error)) isValid = false;
+                }
+                if (isValid)   // валидация успешна
+                {
+                    var user = new DAL.Entities.User();
+                    // крипто-соль - это случайное число (в сроковом виде)
+                    user.PassSalt = _hasher.Hash(DateTime.Now.ToString());
+                    user.PassHash = _hasher.Hash(
+                        UserData.Password1 + user.PassSalt);  // соль "смешивается" с паролем
+                    user.Avatar = UserData.Avatar.FileName;   // заменить по результатам ДЗ
+                    user.Email = UserData.Email;
+                    user.RealName = UserData.RealName;
+                    user.Login = UserData.Login;
+                    user.RegMoment = DateTime.Now;
+
+                    // добавляем в БД (контекст)
+                    _introContext.Users.Add(user);
+
+                    // сохраняем изменения
+                    _introContext.SaveChanges();
+                }
+            }
+            ViewData["UserData"] = UserData;
+            ViewData["err"] = err;
+            return View("Register");
+        }
+
+        public IActionResult Data()
+        {
+            return Json(new { field = "value" });
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View(
+                new ErrorViewModel { 
+                    RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier 
+                });
         }
     }
 }
+/*
+ * Д.З. Вывести на стартовой странице всех
+ * зарегистрированных в БД пользователей (только RealName)
+ */
